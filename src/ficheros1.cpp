760aea6b43523dea8bea7cc57ab7b77e51af91c0
MadbaM <jdloferro@gmail.com>
2015-04-28 16:53:57 +0200

    Final

--- src/ficheros1.cpp
+++ src/ficheros1.cpp
@@ -6,10 +6,28 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
-using namespace std;
+/*Creacion de un fichero secuencial*/
+#include <stdio.h>
+FILE *pcoche;
+int main()
+{
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+char modelo[30]="SEAT-TOLEDO" ;
+int precio;
+
+if ((pcoche=fopen("coches.bin","w+b"))==NULL)
+     printf("El fichero no se puede abrir\n") ;
+else
+     {
+         for(precio=100000;precio<=1000000;precio+=100000)
+         	fprintf(pcoche,"%s %d\n",modelo,precio);
+            /* es importante poner %s espacio %d
+              porque la función fscanf se comporta com scanf
+              por eso se pone un espacio como separador de datos dentro
+              del archivo sino al leer lee toda la cadena junta es decir
+              leería la cadena y el entero*/
+          fclose(pcoche);
+     }
+return 0;
 }
+
